e612f737ecd37873d1e77644677b9ebd06a07c1f
Nick <podewils08@gmail.com>
2022-11-25 10:30:00 +0100

    Change health, sett,add,subt

--- Entities.py
+++ Entities.py
@@ -9,13 +9,14 @@ class Player():
     #inv = [["Waffen"],["Rüstung"],["Usables"],["Questitems"],["Misc"]]
     #type = Tank, Thief, Elf, Ork, Dwarf, Hurensohn etc.
 
-    def __init__(self, name, health, wealth, xp, inv, ptype):
+    def __init__(self, name, health, wealth, xp, inv, ptype, effects):
         self.name = name
         self.health = health
         self.wealth = wealth
         self.xp = xp
         self.inv = inv
         self.ptype = ptype
+        self.effects = effects
 
     def set_name(self):
         while True:
@@ -24,3 +25,15 @@ class Player():
             action = pr.inp("[DIESE EINSTELLUNG KANNST DU NICHT RÜCKGÄNGIG MACHEN!](y/n)")
             if action == "y":
                 break
+    
+    def change_health(self, set=False, add=True, subt=False, value=0):
+        if set == True:
+            self.health = value
+
+        if add == True:
+            self.healt = self.health + value
+
+        if subt == True:
+            self.health = self.health - value
+        
+
