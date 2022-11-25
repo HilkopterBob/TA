d458829951b4fa5427b43cf46973475b0e93e6dd
Nick <podewils08@gmail.com>
2022-11-25 20:32:00 +0100

    cleaning

--- Utils.py
+++ Utils.py
@@ -3,7 +3,6 @@ from pystyle import Write, Colors
 def n(text):
     Write.Print(text + "\n", Colors.red_to_purple, interval=0.0025)
 
-#verinfachte Printanweisung in roter farbe für alerts
 def a(text):
     Write.Print(text + "\n", Colors.red, interval=0.0025)
     
