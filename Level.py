1eb2fd1340acdfd4cf6ed0e0aecfa315c4ee3700
Nick <podewils08@gmail.com>
2022-11-25 10:00:56 +0100

    Infastruktur für Levelklasse

--- Level.py
+++ Level.py
@@ -1,4 +1,10 @@
 class Level():
 
-    def __init__(self):
+    def __init__(self, text, choices, name="Levelnameplatzhalter", inv=[], ltype="Testtype"):
+        self.name = name
+        self.inv = inv
+        self.ltype = ltype
+        self.texts
+
+    def show_content():
         pass
