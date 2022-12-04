5420ed2c4b23879607f21ec86e3781993938b387
Nick <podewils08@gmail.com>
2022-12-04 21:27:39 +0100

    load effects from json

--- Effect.py
+++ Effect.py
@@ -1,4 +1,5 @@
 import Utils as pr
+import json
 
 
 
@@ -11,4 +12,44 @@ class Effect():
         self.value = value
         self.infl = influenced              #:Influenced Vale, welcher Wert beeinflusst wird (hp, xp, atk, def, speed etc)
 
+    @staticmethod   #Generate Level from Json
+    def from_json(json_dct, ename):
+        return Effect(ename, json_dct["descr"], json_dct["etype"], json_dct["value"], json_dct["infl"])
+
+
     
+class EffectInit():
+    def load_all_effects_from_json(json_file):
+        """
+            Return alls Effects from Json file
+            
+            :json_file (File): Json file to load Effects from
+
+            =return= List of all Effects loaded from Json
+        """
+        effects_master_list = []
+        with open(json_file, encoding="UTF-8") as json_data:
+            data = json.load(json_data)
+            
+        for ename in data.keys():
+            effects_master_list.append(Effect.from_json(data[ename], ename))
+        return effects_master_list
+
+    def load_effect_by_name_from_json(json_file, name):
+        """
+            Return a single Level Object from Json by given Name
+            
+            :json_file (File): Json File to load Item from
+
+            =return= Level object
+        """
+        with open(json_file, encoding="UTF-8") as json_data:
+            data = json.load(json_data)
+            
+
+        for ename in data.keys():
+            if ename == name:
+                return Effect.from_json(data[ename], ename)
+        
+        pr.dbg(f"Levelname: {pr.cyan(ename)} not found!", 1)
+        return False
