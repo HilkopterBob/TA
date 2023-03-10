cdb6b5c9dd032ea4ea6c8db570dfd5349004eb4b
Nick <podewils08@gmail.com>
2023-03-10 23:05:01 +0100

    rework compatibility * import

--- config/__init__.py
+++ config/__init__.py
@@ -1,3 +1,3 @@
 from .config import dbg, items_file, levels_file, effects_file, entity_file
 
-__all__ = [dbg, items_file, levels_file, effects_file, entity_file]
+__all__ = ["dbg", "items_file", "levels_file", "effects_file", "entity_file"]
