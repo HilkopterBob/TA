2d335d7365a057c6b3d7dbfa98d7afe267c99d4a
Nick <podewils08@gmail.com>
2023-03-10 21:16:52 +0100

    Hinzufügen init.py für packaging support

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from .actionparser import Actionparser
+from .Effect import Effect, EffectInit
+from .Entities import Entity, EntityInit, item, itemInit
+from .Inputpraser import inputphraser
+from .Level import Level, LevelInit
+from .Utils import *
