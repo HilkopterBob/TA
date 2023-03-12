3bf889a695313eb67c90944e2afefa09de24445b
Kartoffel096 <gollum096@gmail.com>
2023-03-12 02:34:30 +0100

    Refactored Inputparser.py for Linting

--- Inputpraser.py
+++ Inputpraser.py
@@ -1,4 +1,18 @@
-def inputphraser(user_input, min=5, max=50):
+"""
+Inputparser Module which holds 1 Function
+    inputparser()
+"""
+def inputparser(user_input, minl=5, maxl=50):
+    """Parses the User Inputs
+
+    Args:
+        user_input (String): Input from user
+        minl (int, optional): Min length of String. Defaults to 5.
+        maxl (int, optional): Max length of String. Defaults to 50.
+
+    Returns:
+        String: user_input
+    """
     befehlszeichen = "#"
 
 
@@ -6,50 +20,41 @@ def inputphraser(user_input, min=5, max=50):
 
         if not user_input.strip():
             raise ValueError("Leere Eingabe")
-        
-        if len(user_input) < min:
-            raise ValueError(f"Der Input muss mindestens {min} Zeichen lang sein")
-        
-        if len(user_input.strip()) > max:
-            raise ValueError(f"Der Input darf nicht länger als {max} Zeichen sein")
-        
+
+        if len(user_input) < minl:
+            raise ValueError(f"Der Input muss mindestens {minl} Zeichen lang sein")
+
+        if len(user_input.strip()) > maxl:
+            raise ValueError(f"Der Input darf nicht länger als {maxl} Zeichen sein")
+
         #Wenn Eingabe == Zahl dann
-        if (user_input.isdigit()):
+        if user_input.isdigit():
             user_input = int(user_input)
 
         #Wenn Eingabe == String dann
         else:
             user_input = str(user_input)
             user_input = user_input.lower() #alles kleinbuchstaben
-            
+
             #befehlserkennung
-            if (user_input[0]==befehlszeichen): 
+            if user_input[0]==befehlszeichen:
                 return user_input[1:]
-            
+
             print(f"Ich bin ein Text: {user_input}")
 
         return user_input
-    
+
     except ValueError as e:
         print ("Fehler bei Eingabe:", e)
         return None
     except:
         print ("Unbekannter Fehler beim Input")
         return None
-        
-    
-user_input = input("Geben Sie eine Benutzereingabe ein: ")
-processed_input = inputphraser(user_input, min=2, max=10)
+
+
+inputstr = input("Geben Sie eine Benutzereingabe ein: ")
+processed_input = inputparser(inputstr, minl=2, maxl=10)
 if processed_input is not None:
     print(processed_input)
-    print(type(user_input))
+    print(type(inputstr))
     print(type(processed_input))
-
-#befehlserkennung
-#nur zalen/text erlaubt
-
-
-
-
-
-
