e02f50516240cf3bf8196bfe97d3aff2e7fd9d77
Kartoffel096 <gollum096@gmail.com>
2023-03-12 02:36:12 +0100

    Renamed Files

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from .actionparser import Actionparser
 from .Effect import Effect, EffectInit
 from .Entities import Entity, EntityInit, item, itemInit
-from .Inputpraser import inputphraser
+from .Inputparser import inputparser
 from .Level import Level, LevelInit
 from .Utils import pr, inp, Debug
 from config import *
