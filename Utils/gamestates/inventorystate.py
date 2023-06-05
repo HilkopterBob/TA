2996eda795c874668c66789e0a59fadd6b1b41ff
Nick <podewils08@gmail.com>
2023-06-05 13:18:17 +0200

    added consume_item and equip_item

--- Utils/gamestates/inventorystate.py
+++ Utils/gamestates/inventorystate.py
@@ -13,53 +13,9 @@ Gamelogic: Was wird actionpaser und was direkt abgeabreitet?
 
 def inventorystate(Player):
 
-    def consume_item(item_name, Player):
-        for item in Player.inv:
-            if item.name == item_name:
-                consumable = item
+    
 
-        if consumable.effects and consumable.type != "Food":
-            Player.remove_item_by_name(consumable.name)
-            for effect in consumable.effects:
-                Player.add_effect(effect)
 
-        if consumable.type == "Food":
-            Player.change_health(consumable.dmg)
-
-    def equip_item(item_name, Player):
-        
-        for item in Player.inv:
-            if item.name == item_name:
-                cur_item = item
-        
-        #["Head_slot", "Torso_slot", "Underwear", "Left_arm", "Right_arm", 
-        #"Left_leg", "Right_leg", "Gloves_slot", "Meele Weapon", "Ranged Weapon", 
-        #"Quick_draw potion"]
-
-        
-        match cur_item.equip_slot:
-            case "head":
-                Player.equip_slot[0] = cur_item
-            case "torso":
-                Player.equip_slot[1] = cur_item
-            case "underwear":
-                Player.equip_slot[2] = cur_item
-            case "left_arm":
-                Player.equip_slot[3] = cur_item
-            case "right_arm":
-                Player.equip_slot[4] = cur_item
-            case "left_leg":
-                Player.equip_slot[5] = cur_item
-            case "right_leg":
-                Player.equip_slot[6] = cur_item
-            case "gloves":
-                Player.equip_slot[7] = cur_item
-            case "meele":
-                Player.equip_slot[8] = cur_item
-            case "ranged":
-                Player.equip_slot[9] = cur_item
-            case "quick_draw_potion":
-                Player.equip_slot[10] = cur_item
             
 
 
