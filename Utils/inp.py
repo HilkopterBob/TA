dc44afdf4fcb9f9cd9ab18fbd5d40eca32cfc409
Tim16-sys <66381348+Tim16-sys@users.noreply.github.com>
2023-07-11 13:45:17 +0200

    Update inp.py

--- Utils/inp.py
+++ Utils/inp.py
@@ -120,13 +120,13 @@ class Inp:
 
                     case "changehealth" | "ch":
                         cvalue = int(input_list[1])
-                        player.change_health (cvalue)
+                        player.change_health (cvalue)    # pylint: disable=E1101
 
                     case "kill":
-                        player.change_health (-player.hp)
+                        player.change_health (-player.hp)    # pylint: disable=E1101
 
                     case "god" | "tgm" | "gm1":
-                        player.change_health (10000)
+                        player.change_health (10000)    # pylint: disable=E1101
 
                     # User Befehle
 
