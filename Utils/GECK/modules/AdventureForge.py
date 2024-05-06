1da03f5cb47882a1edf6f0bd0618df2482c3a7ca
Nick von Podewils <n.podewils@schwerin-sicherheit.de>
2024-05-06 09:10:20 +0200

    add: TAE-AF blessed app

--- Utils/GECK/modules/AdventureForge.py
+++ Utils/GECK/modules/AdventureForge.py
@@ -16,47 +16,65 @@
 # TODO: dump to json
 # TODO: go back to main
 
-from textual.app import App, ComposeResult
-from textual.screen import Screen
-from textual.widgets import Static, Markdown, Button
+from blessed import Terminal
 
-TITLE = """\
-# AdventureForge
-
-## The Text Adventure Engine Editor
-"""
+term = Terminal()
 
 
-class LevelEditor(Screen):
-    """Level-Editor Screen"""
-
-    BINDINGS = [("escape", "app.pop_screen", "Pop screen")]
-
-    def compose(self) -> ComposeResult:
-        yield Static(" Windows ", id="title")
-        yield Static("Press any key to continue [blink]_[/]", id="any-key")
+# Leveleditor-Funktion
+def level_editor():
+    print(term.clear)
+    print(term.center(term.bold("Leveleditor")))
+    term.inkey()  # Hier kann die Funktionalität des Leveleditors implementiert werden
 
 
-class AdventureForge(App):
-    """Main-App Framework"""
+# Effecteditor-Funktion
+def effect_editor():
+    print(term.clear)
+    print(term.center(term.bold("Effecteditor")))
+    term.inkey()  # Hier kann die Funktionalität des Effecteditors implementiert werden
 
-    # BINDINGS = [("b", "push_screen('bsod')", "BSOD")]
 
-    CSS_PATH = "AdventureForge.tcss"
+# Itemeditor-Funktion
+def item_editor():
+    print(term.clear)
+    print(term.center(term.bold("Itemeditor")))
+    term.inkey()  # Hier kann die Funktionalität des Itemeditors implementiert werden
 
-    def compose(self) -> ComposeResult:
-        yield Markdown(TITLE, id="title")
-        yield Button("Level Editor!", variant="primary", classes="box")
-        yield Button("Item Editor!", variant="primary", classes="box")
-        yield Button("Entity Editor!", variant="primary", classes="box")
-        yield Button("Effect Editor!", variant="primary", classes="box")
 
-    def on_mount(self) -> None:
-        """mount screens at runtime"""
-        # self.install_screen(Mainscreen(), name="Mainscreen")
-        self.install_screen(LevelEditor(), name="LevelEditor")
+# Entityeditor-Funktion
+def entity_editor():
+    print(term.clear)
+    print(term.center(term.bold("Entityeditor")))
+    term.inkey()  # Hier kann die Funktionalität des Entityeditors implementiert werden
+
+
+def main():
+    while True:
+        print(term.clear)
+        print(term.center(term.blink("AdventureForge")))
+        print(term.center(term.italic("Hauptmenü")))
+        print()
+        print("")
+        print("1. Leveleditor")
+        print("2. Effecteditor")
+        print("3. Itemeditor")
+        print("4. Entityeditor")
+        print("0. Beenden")
+
+        with term.cbreak():
+            val = term.inkey()
+            if val == "1":
+                level_editor()
+            elif val == "2":
+                effect_editor()
+            elif val == "3":
+                item_editor()
+            elif val == "4":
+                entity_editor()
+            elif val == "0":
+                break
 
 
 if __name__ == "__main__":
-    app = AdventureForge()
-    app.run()
+    main()
